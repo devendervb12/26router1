f76e37eb4a3b7c5747f27c56759756afb51d64f3
devendervb12 <info@s-max.in>
2017-12-15 07:21:32 +0530

    adding manifest files, adding datasource at component level

--- WebContent/Component.js
+++ WebContent/Component.js
@@ -1,35 +1,13 @@
 
 sap.ui.core.UIComponent.extend("smax.batch26.Component", {	
 	metadata : {
-		rootView : "smax.batch26.view.App",
-		
-		routing : {
-			config : {
-				routerClass : "sap.m.routing.Router",
-				controlId : "idApp",
-				controlAggregation : "pages",
-				viewType : "XML",
-				viewPath : "smax.batch26.view"
-			},
-			routes : [
-				{
-					pattern : "", 
-					name : "p1",
-					viewName : "Page1"
-				},
-				{
-					pattern : "pp2",
-					name : "p2",
-					viewName : "Page2"
-				}
-			]
-		}		
+		manifest : "json"		
 	},	
 	init : function(){		
 		// initialize with parent properties
 		sap.ui.core.UIComponent.prototype.init.apply(this);
-		
 		this.getRouter().initialize();
+		
 	}
 	
 });
--- WebContent/view/Page1.controller.js
+++ WebContent/view/Page1.controller.js
@@ -1,5 +1,6 @@
 sap.ui.controller("smax.batch26.view.Page1", {
 
+	
 	gotoPage2 : function(){
 		// goto page2
 		var oRouter = this.getOwnerComponent().getRouter();
